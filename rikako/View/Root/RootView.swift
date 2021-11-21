40b31304239b931f80feca8e62376a67a9ead0f1
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2021-11-22 02:09:55 +0900

    Top周りの諸々修正

--- rikako/View/Root/RootView.swift
+++ rikako/View/Root/RootView.swift
@@ -3,9 +3,6 @@ import SwiftUI
 struct RootView: View {
     @EnvironmentObject var appEnviroment: RikakoAppEnvironment
     
-    init() {
-        UITabBar.appearance().barTintColor = UIColor.blue
-    }
     var body: some View {
         if appEnviroment.doneTutorial {
             TopView()
